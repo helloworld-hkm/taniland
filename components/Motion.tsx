0ebf948e97f7239607977424eb8f3e79c9145da5
Hakim Firman <hakimastina@gmail.com>
2023-12-19 04:23:33 +0700

    Update Motion.tsx

--- components/Motion.tsx
+++ components/Motion.tsx
@@ -1,27 +1,36 @@
-/* eslint-disable */
-import { motion, useInView } from "framer-motion";
-import { useRef } from "react";
-export const Motion = ({ children,direction, transition }) => {
-    // Motion Bottom to Top
-    const ref = useRef(null);
-    const isInView = useInView(ref, { once: true });
+import { useRef, ReactNode } from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-    const elementVariants = {
-        hidden: { y: direction==='up'?50 :direction==='down'? -50 :0,
-        opacity:0,
-        x: direction==='left'?50 :direction==='right'? -50 :0, },
-        visible: { opacity: 1, y: 0 }
-    };
+interface MotionProps {
+  children: ReactNode;
+  direction: any; // Update with your actual type
+  transition: any; // Update with your actual type
+}
 
-    return (
-        <motion.div
-            variants={elementVariants}
-            initial="hidden"
-            animate={isInView ? "visible" : "hidden"}
-            transition={transition}
-            ref={ref}
-        >
-            {children}
-        </motion.div>
-    );
+export const Motion: React.FC<MotionProps> = ({ children, direction, transition }) => {
+  // Motion Bottom to Top
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const elementVariants = {
+    hidden: {
+      y: direction === "up" ? 50 : direction === "down" ? -50 : 0,
+      opacity: 0,
+      x: direction === "left" ? 50 : direction === "right" ? -50 : 0,
+    },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  return (
+    <motion.div
+      variants={elementVariants}
+      initial="hidden"
+      animate={isInView ? "visible" : "hidden"}
+      transition={transition}
+      ref={ref}
+    >
+      {children}
+    </motion.div>
+  );
 };
