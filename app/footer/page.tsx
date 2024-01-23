b502be83440e59d4c8c87f92f3e5edb5897256ff
Hakim Firman <hakimastina@gmail.com>
2024-01-23 17:42:11 +0700

    update eslint

--- app/footer/page.tsx
+++ app/footer/page.tsx
@@ -25,7 +25,7 @@ const Footer = () => {
       .catch((error) => console.error('Error fetching access count:', error));
        
   
-  });
+  }, []);
 
 //   const incrementAccessCount = () => {
 //     fetch('https://my-json-server.typicode.com/helloworld-hkm/visitor/guest/1', {
